8505e9154cfb7f9f080982365a27c8c90ca8f380
David Yallup <david.yallup@gmail.com>
2022-07-15 11:53:18 +0100

    fix rastrigin

--- rastrigin.py
+++ rastrigin.py
@@ -9,24 +9,23 @@ def rastrigin(x,scale):
     #vectorized 2d rastrigin function
     x1=x[...,0]
     x2=x[...,1]
-    return -(2*scale + np.sum(x**2- scale * np.cos(2*np.pi*x),axis=-1))
+    return (20*scale - np.sum(x**2- scale * np.cos(2*np.pi*x),axis=-1))
     # return -((x1**2+x2-11)**2 + (x1+x2**2-7)**2)/(2*scale**2)
 
 #make a grid in mpl
 grid = np.mgrid[-5:5:.1, -5:5:.1]
 gridstack= np.dstack(grid)
 
-#this scale
-scale=10
-#we want the function to be the loglikelihood, so exp it to get the true target function
-z=np.exp(rastrigin(gridstack,scale))
-
+#scale adjusts depth of "eggbox"
+scale=5
+#rastrigin ~cosine, so we don't want the rastrigin as the logl, rather as the target
+z=rastrigin(gridstack,scale)
 target_directory="rastrigin"
 
 #wrap up likelihood and prior, plys set polychord settings
 prior = priors.UniformPrior(-5,5)
 def loglike(theta):
-    return float(rastrigin(theta,scale)), []
+    return float(np.log(rastrigin(theta,scale))), []
 
 settings=settings.PolyChordSettings(nDims=2,nDerived=0)
 #rastrigin needs a lot of live points
