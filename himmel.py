6df36455c4208f66fe4523395a7922bfbc0aa910
David Yallup <david.yallup@gmail.com>
2022-11-28 15:59:18 +0000

    update example animation with compression

--- himmel.py
+++ himmel.py
@@ -28,12 +28,34 @@ def loglike(theta):
 settings=settings.PolyChordSettings(nDims=2,nDerived=0)
 settings.nlive=100
 settings.base_dir=os.path.join(target_directory,"chains")
-run_polychord(loglike,prior=prior,nDims=2,nDerived=0,settings=settings)
+# run_polychord(loglike,prior=prior,nDims=2,nDerived=0,settings=settings)
 
 #read in nested samples
-ns=anesthetic.NestedSamples(root=os.path.join(target_directory,"chains/test"))
-
+# ns=anesthetic.NestedSamples(root=os.path.join(target_directory,"chains/test"))
+ns=anesthetic.read_chains(root=os.path.join(target_directory,"chains/test"))
 #make the live points animation
 make_animation_frames(grid,z,ns,target_directory)
 #plot the dead points 
 plot_dead(grid,z,ns,target_directory)
+
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+plt.style.use("prb")
+mpl.rcParams['grid.alpha'] = 0.2
+mpl.rcParams['grid.color'] = "gray"
+X,Y=grid
+fig = plt.figure(figsize=[5,4])
+ax = plt.axes(projection='3d')
+# ax.contour3D(X,Y, z, 10, cmap='magma')
+ax.plot_surface(X, Y, z, rstride=1, cstride=1,
+                cmap='magma', edgecolor='none')
+# plt.axis('off')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+ax.set_zlabel(r'$f(x)$')
+ax.grid(alpha=0.2)
+# plt.grid(b=None)
+ax.view_init(45, 35)
+fig.savefig(os.path.join(target_directory,"himmelblau.pdf"))
+fig.savefig(os.path.join(target_directory,"himmelblau.png"),dpi=250)
--- plotting/plots.py
+++ plotting/plots.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 import subprocess
 
-plt.style.use(os.path.dirname(os.path.realpath(__file__))+'/polynet.mplstyle')
-
+# plt.style.use(os.path.dirname(os.path.realpath(__file__))+'/polynet.mplstyle')
+plt.style.use("prb")
 
 def make_animation_frames(grid,z,samples,filename,frames_dir="frames"):
     """Make animation frames then animate them with subprocess call to convert
@@ -17,8 +17,8 @@ def make_animation_frames(grid,z,samples,filename,frames_dir="frames"):
         frames_dir (str, optional): Defaults to "frames".
     """
     anim_dir=os.path.join(filename,frames_dir)
-    gs = {"wspace": 0.05, "hspace": 0.03}
-    n_frames=30
+    gs = {"wspace": 0.15, "hspace": 0.03}
+    n_frames=20
     # n_samp=500
     os.makedirs(anim_dir,exist_ok=True)
 
@@ -26,13 +26,13 @@ def make_animation_frames(grid,z,samples,filename,frames_dir="frames"):
         uw = samples.logL_birth[~(samples.logL_birth == -np.inf)]
         points = samples.live_points(uw.min() + i / n_frames * (uw.max() - uw.min()))
 
-        f, a = plt.subplots(nrows=1, ncols=2, sharey=True,sharex=True, gridspec_kw=gs, figsize=[8, 4])
+        f, a = plt.subplots(nrows=1, ncols=2, sharey=True,sharex=True, gridspec_kw=gs, figsize=[6, 3])
         scatterps=points #points.sample(100,replace=True)
         a[0].contour(*grid, z, levels=10, alpha=1.,cmap="magma")
         a[0].contour(*grid, z, levels=[np.exp(scatterps.logL.min())], colors="black", alpha=.6, linewidths=3)
         
-        a[0].set_title("Target Function")
-        a[1].set_title("Nested Sampling live points")
+        a[0].set_title("Likelihood",fontsize=6)
+        a[1].set_title("Live points",fontsize=6)
         
         a[1].scatter(scatterps.to_numpy()[...,0],scatterps.to_numpy()[...,1],marker="o",color="C0",edgecolor="black",rasterized=True)
         a[1].contour(*grid, z, levels=[np.exp(scatterps.logL.min())], colors="black", alpha=.6, linewidths=3)
@@ -40,15 +40,18 @@ def make_animation_frames(grid,z,samples,filename,frames_dir="frames"):
         a[0].set_ylim(-5, 5)
 
         # f.tight_layout()
-        f.savefig(os.path.join(anim_dir, str(i) + ".png"),dpi=150)
+        f.savefig(os.path.join(anim_dir, str(i) + ".png"),dpi=300)
 
     
-    proc=subprocess.Popen(["convert", "-delay", "50",*[os.path.join(anim_dir,str(x)+".png") for x in np.arange(n_frames-1)],"-delay","250",os.path.join(anim_dir,str(n_frames-1)+".png"),os.path.join(filename,filename)+".gif"])
+    proc=subprocess.Popen(["convert", "-delay", "100",*[os.path.join(anim_dir,str(x)+".png") for x in np.arange(n_frames-1)],"-delay","300",os.path.join(anim_dir,str(n_frames-1)+".png"),os.path.join(filename,filename)+".gif"])
     proc.communicate()
-    
+    proc=subprocess.Popen(["mogrify", "-layers", "optimize", "-fuzz", "7%", os.path.join(filename,filename)+".gif"])
+    proc.communicate()
+
+
 def plot_dead(grid,z,samples,filename):
     gs = {"wspace": 0.05, "hspace": 0.03}
-    f, a = plt.subplots(nrows=1, ncols=2, sharey=True,sharex=True, gridspec_kw=gs, figsize=[8, 4])
+    f, a = plt.subplots(nrows=1, ncols=2, sharey=True,sharex=True, gridspec_kw=gs, figsize=[6, 3])
     
     samp=samples[[0,1]].to_numpy()
     a[0].contour(*grid, z, levels=10, alpha=1.,cmap="magma")
